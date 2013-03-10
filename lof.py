67217bb239ff847fff02133ac055468e00f3b5c5
Simon Forman <forman.simon@gmail.com>
2013-03-09 21:48:03 -0800

    Move machine into lof.py

--- lof.py
+++ lof.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from itertools import imap as lazy_apply, ifilter
 from each_way import each_way, each_way_r
-from machine import wrap, unwrap, delete, undelete, copy, uncopy, apply_
 '''
 
 "The Laws of Form" by G. Spencer-Brown.
@@ -112,6 +111,115 @@ def reify(form, meaning):
   return tuple(reify(inner, meaning) for inner in form)
 
 
+def wrap(form, i):
+  '''
+
+  * -> ((*))
+
+  In order to handle the wrapping of nothing in-between the inner forms
+  of the form we adopt the convention that even i (0, 2, 4, ...) will
+  select the intersticies of the inner forms, while the odd i (1, 3, ...)
+  will select the inner forms themselves.
+
+  Therefore the valid indicies for a form g are: range(2 * len(g) + 1).
+  Greater indicies will append wrap the nothing at the end of the form.
+
+  If you wrap (0, 1, 2) with the following indicies you get these
+  resultant forms:
+
+    0 -> (((),), 0, 1, 2)
+    1 -> (((0,),), 1, 2)
+    2 -> (0, ((),), 1, 2)
+    3 -> (0, ((1,),), 2)
+    4 -> (0, 1, ((),), 2)
+    5 -> (0, 1, ((2,),))
+    6 -> (0, 1, 2, ((),))
+
+  '''
+  wrap_nothing = not i % 2
+  i /= 2
+  left = form[:i]
+  if wrap_nothing:
+    right = form[i:]
+    t = ((),)
+  else:
+    right = form[i + 1:]
+    t = ((form[i],),)
+  return left + (t,) + right
+
+
+def unwrap(form, i):
+  '''
+
+  ((*)) -> *
+
+  Unwrap an inner form (including nothing) in form at index i.
+
+  (Unlike the wrap() function which handles indices specially to permit
+  one function to wrap both "present" forms and intersticial nothing,
+  unwrap() uses just the normal index.
+
+
+    0 -> (((),), 0, 1, 2) -> (0, 1, 2)
+    0 -> (((0,),), 1, 2) -> (0, 1, 2)
+    1 -> (0, ((),), 1, 2) -> (0, 1, 2)
+    1 -> (0, ((1,),), 2) -> (0, 1, 2)
+    2 -> (0, 1, ((),), 2) -> (0, 1, 2)
+    2 -> (0, 1, ((2,),)) -> (0, 1, 2)
+    3 -> (0, 1, 2, ((),)) -> (0, 1, 2)
+
+  '''
+  left, t, right = form[:i], form[i], form[i + 1:]
+  return (left + right) if t == ((),) else (left + (_k(t),) + right)
+
+# Helper function to "unwrap" a single wrapped form.
+_k = lambda (((anything,),)): anything
+
+
+def delete(form, i):
+  '''
+  OA -> O
+  '''
+  assert form[i] == ()
+  i += 1
+  return form[:i] + form[i + 1:]
+
+
+def undelete(form, i, anything):
+  '''
+  O -> OA
+  '''
+  assert form[i] == ()
+  i += 1
+  return form[:i] + (anything,) + form[i:]
+
+
+def copy(form, i):
+  '''
+  A(B) -> A(AB)
+  '''
+  left, A, t, right = form[:i + 1], form[i], form[i + 1], form[i + 2:]
+#  print 'copy', left, A, t, right
+  return left + ((A,) + t,) + right
+
+
+def uncopy(form, i):
+  '''
+  A(AB) -> A(B)
+  '''
+  left, A, t, right = form[:i], form[i], form[i + 1], form[i + 2:]
+  assert t[0] == A
+  return left + (t[1:],) + right
+
+
+def apply_(form, program):
+  if program and callable(program[0]):
+    function, args = program[0], program[1:]
+    return function(form, *args)
+  for inner in program:
+    form = apply_(form, inner)
+  return form
+
 
 Instructions = {
   ((number_to_form(1),),): wrap,
@@ -121,6 +229,7 @@ Instructions = {
   ((number_to_form(5),),): copy,
   ((number_to_form(6),),): uncopy,
   }
+
 
 Instructions_reversed = {
   'wrap': ((number_to_form(1),),),
