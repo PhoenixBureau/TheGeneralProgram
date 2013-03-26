7a9e12830fcef66575ad74e6fe73a4fa1811b040
Simon Forman <forman.simon@gmail.com>
2013-03-26 13:34:43 -0700

    Fun with recursion.

--- ebt.py
+++ ebt.py
@@ -6,6 +6,8 @@ Based on "Functional Pearl: Every Bit Counts"
 by Dimitrios Vytiniotis & Andrew J. Kennedy
 https://dl.acm.org/citation.cfm?id=1863548
 '''
+from operator import sub, add
+
 
 a = lambda n: n == 0
 b = lambda n: n == 1
@@ -21,3 +23,21 @@ def decode(game, value):
   if isinstance(it, tuple):
     return decode(it, value)
   return it
+
+
+def F(N, op=sub, arg=1):
+  if not N: return N
+  return (
+    lambda n: n == N,
+    (N, F(op(N, arg), op, arg))
+    )
+
+
+if __name__ == '__main__':
+  V = F(3)
+  for n in range(-2, 7): print decode(V, n),
+  print
+
+  B = F(-3, add, 1)
+  for n in range(-4, 1): print decode(B, n),
+  print
