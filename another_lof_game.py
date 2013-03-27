71af78fa018bea006ae1a0a77dd40e28ea42b31a
Simon Forman <forman.simon@gmail.com>
2013-03-27 12:10:37 -0700

    Just the bits, please.

--- another_lof_game.py
+++ another_lof_game.py
@@ -4,27 +4,23 @@ def encode(game, value):
   '''
   Return the path that encodes the value according to the game.
   '''
-  next_predicate = game
   path = []
-  while True:
-    bits, next_predicate = next_predicate(value)
-    path += map(int, bits)
-    if not next_predicate:
-      break
+  bits = game(value)
+  path += map(int, bits)
   return path
 
 def a_mark(value):
   if value == ():
-    return [0], None
-  bits, f = A(value)
-  return [1] + bits, f
+    return [0]
+  bits = A(value)
+  return [1] + bits
 
 def A(value):
   bits = encode(a_mark, value[0])
   if len(value) == 1:
-    return [0] + bits, None
+    return [0] + bits
   bits += encode(a_mark, value[1:])
-  return [1] + bits, None
+  return [1] + bits
 
 if __name__ == '__main__':
   for form in (
