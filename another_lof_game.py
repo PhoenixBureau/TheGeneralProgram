eb4024d5b16ac2fb7bd192a4d534863e576fd9f5
Simon Forman <forman.simon@gmail.com>
2013-03-27 13:51:29 -0700

    Encode to strings (of '0' and '1').

--- another_lof_game.py
+++ another_lof_game.py
@@ -4,20 +4,20 @@ def encode(game, value):
   '''
   Return the path that encodes the value according to the game.
   '''
-  return map(int, game(value))
+  return game(value)
 
 def a_mark(value):
   if value == ():
-    return [0]
+    return '0'
   bits = A(value)
-  return [1] + bits
+  return '1' + bits
 
 def A(value):
   bits = encode(a_mark, value[0])
   if len(value) == 1:
-    return [0] + bits
+    return '0' + bits
   bits += encode(a_mark, value[1:])
-  return [1] + bits
+  return '1' + bits
 
 if __name__ == '__main__':
   q, p = {}, {}
@@ -36,10 +36,15 @@ if __name__ == '__main__':
     ((((),),),),
     ):
 
-    path = ''.join(map(str, encode(a_mark, form)))
+    path = encode(a_mark, form)
     q[path] = form
     p[form] = path
 
-    print '%-28s -> %s' % (form, path)
+    print '%-28s -> %s' % (form, path),
+    if path.endswith('0100'):
+      print '~>', path[:-4] + 'x'
+    else:
+      print
+
 ##    print '_' * 70
 
