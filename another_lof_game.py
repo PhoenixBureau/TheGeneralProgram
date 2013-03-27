e363889a488e7f38d49f95fd5f4a0bff3b2cdbfd
Simon Forman <forman.simon@gmail.com>
2013-03-27 13:53:26 -0700

    encode() was redundant.

--- another_lof_game.py
+++ another_lof_game.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
 
-def encode(game, value):
-  '''
-  Return the path that encodes the value according to the game.
-  '''
-  return game(value)
-
 def a_mark(value):
   if value == ():
     return '0'
@@ -13,10 +7,10 @@ def a_mark(value):
   return '1' + bits
 
 def A(value):
-  bits = encode(a_mark, value[0])
+  bits = a_mark(value[0])
   if len(value) == 1:
     return '0' + bits
-  bits += encode(a_mark, value[1:])
+  bits += a_mark(value[1:])
   return '1' + bits
 
 if __name__ == '__main__':
@@ -36,7 +30,7 @@ if __name__ == '__main__':
     ((((),),),),
     ):
 
-    path = encode(a_mark, form)
+    path = a_mark(form)
     q[path] = form
     p[form] = path
 
