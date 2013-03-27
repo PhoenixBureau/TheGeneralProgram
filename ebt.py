a779f618d034199e90a779ce1ae4f4dd08f3488f
Simon Forman <forman.simon@gmail.com>
2013-03-26 18:41:58 -0700

    Trying some stuff.

--- ebt.py
+++ ebt.py
@@ -7,22 +7,48 @@ by Dimitrios Vytiniotis & Andrew J. Kennedy
 https://dl.acm.org/citation.cfm?id=1863548
 '''
 from operator import sub, add
+from pprint import pprint
 
 
-a = lambda n: n == 0
-b = lambda n: n == 1
-c = lambda n: n == 2
+def encode(game, value, path=None):
+  if path is None:
+    path = []
 
-
-G = (a, (0, 1))
-
-
-def decode(game, value):
   predicate, choices = game
-  it = choices[not predicate(value)]
+
+  P = not predicate(value) # Invert to index the branches.
+                           # True takes left
+                           # False takes right branch.
+  path.append(int(P))
+
+  it = choices[P]
+
   if isinstance(it, tuple):
-    return decode(it, value)
-  return it
+    return encode(it, value, path)
+
+  if it == value:
+    return path
+  return []
+
+
+def decode(game, path):
+  for bit in path:
+    _, choices = game
+    game = choices[bit]
+  return game
+
+
+def list_codes(game, path=None):
+  if path is None:
+    path = []
+  if not isinstance(game, tuple):
+    yield path, game
+  else:
+    f, (l, r) = game
+    for P, leaf in list_codes(l, path[:] + [0]):
+      yield P, leaf
+    for P, leaf in list_codes(r, path[:] + [1]):
+      yield P, leaf
 
 
 def F(N, op=sub, arg=1):
@@ -33,11 +59,53 @@ def F(N, op=sub, arg=1):
     )
 
 
+def O(n, m):
+  if n == m:
+    return n
+  assert n < m, (n, m)
+  midpoint = n + (m - n) / 2
+  f = lambda number: number <= midpoint
+  return f, (O(n, midpoint), O(midpoint + 1, m))
+
+
+def _t(bits):
+  return int(''.join(str(bit) for bit in bits), 2)
+
 if __name__ == '__main__':
+  o = O(0, 16)
+  for p, g in list_codes(o):
+    d = decode(o, p)
+    print '%-2s -> %r = %i -> %s' % (g, p, _t(p), d)
+    assert d == g ,(d, g)
+
+  print '-' * 70
+  o = O(0, 15)
+  for p, g in list_codes(o):
+    d = decode(o, p)
+    print '%-2s -> %r = %i -> %s' % (g, p, _t(p), d)
+    assert d == g ,(d, g)
+
+  print '-' * 70
+  o = O(33, 35)
+  for p, g in list_codes(o):
+    d = decode(o, p)
+    print '%-2s -> %r = %i -> %s' % (g, p, _t(p), d)
+    assert d == g ,(d, g)
+
+
+  print '-' * 70
+  print '-' * 70
+
+
+
   V = F(3)
-  for n in range(-2, 7): print decode(V, n),
+  for n in range(-2, 7):
+    path = encode(V, n)
+    print n, path, decode(V, path) if path else None
   print
 
   B = F(-3, add, 1)
-  for n in range(-4, 1): print decode(B, n),
+  for n in range(-4, 1):
+    path = encode(B, n)
+    print n, path, decode(B, path) if path else None
   print
