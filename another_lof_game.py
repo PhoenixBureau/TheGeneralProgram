aa17c40025f1dccb5287dd63c415eaa2d04c6d0e
Simon Forman <forman.simon@gmail.com>
2013-03-28 12:06:02 -0700

    Unsatifactory attempt at decoder.

--- another_lof_game.py
+++ another_lof_game.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+'''
+
+     _ -> 0           ( "Nothing". )
+    () -> 100         ( Something with nothing next to it or inside it. )
+  (A)B -> 1 B? BA : A ( I.e. path[1] indicates if term B exists, then
+                        term B is encoded if it exists, then term A. )
+
+
+'''
+
 
 def a(value):
   return '1' + b(value) if value else '0'
@@ -10,6 +20,37 @@ def b(value):
   return '1' + bits + a(value[1:])
 
 
+def un_a(path):
+  bit, path = path[0], path[1:]
+  if bit == '0':
+    return None, path
+  return un_b(path)
+
+def un_b(path):
+  bit, path = path[0], path[1:]
+
+  if bit == '0':
+    # nothing for B, the rest of the path specifies A
+    A, rest_of_path = un_a(path)
+    if A is None:
+      return (), rest_of_path
+    return (A,), rest_of_path
+
+  # A then B
+  A, rest_of_path = un_a(path)
+  B, rest_of_path = un_a(rest_of_path)
+  assert not rest_of_path, repr(rest_of_path)
+
+  print A, B
+  if A is None:
+    return ((),) + B, rest_of_path
+  return (A, B), rest_of_path
+
+
+def prp(path):
+  LM = un_a(path)
+  print LM
+  print q[path], '<===>', LM[0], LM[0] == q[path]
 
 
 if __name__ == '__main__':
@@ -34,7 +75,7 @@ if __name__ == '__main__':
     q[path] = form
     p[form] = path
     pr_form = ' '.join(map(str, form))
-    print ('%-' + str(w) + 's -> %s') % (pr_form, path)
+#    print ('%-' + str(w) + 's -> %s') % (pr_form, path)
 
   t = ()
   initial = t,
@@ -60,4 +101,17 @@ if __name__ == '__main__':
 ##    else:
 ##      print
 
-#  print '_' * 70
+  print '_' * 70
+
+path = list(q).pop()
+print path
+prp(path)
+
+((), (((),),), ((((((((),),),),),),),))
+((), ((((),),), (((((((),),),),),),) ))
+
+
+
+
+
+
