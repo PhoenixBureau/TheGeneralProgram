05833577b2f21749d9f59b12cafac2509b52ff4e
Simon Forman <forman.simon@gmail.com>
2013-04-10 20:14:47 -0700

    Little bit of reify()

--- category.py
+++ category.py
@@ -1,3 +1,17 @@
+from itertools import imap
+from pprint import pprint as p
+
+
+mark = lambda form: not form or not any(imap(mark, form))
+
+
+nor = lambda *bits: bits
+or_ = lambda *bits: nor(bits)
+and_ = lambda *bits: tuple(nor(bit) for bit in bits)
+nand = lambda *bits: nor(tuple((nor(bit) for bit in bits)))
+xor = lambda *bits: nor(and_(*bits), nor(*bits))
+
+
 C = set((1, 2, 3))
 hom = {}
 
@@ -26,4 +40,56 @@ def future(m, state, time):
   for _ in range(time):
     state = m.get(state, state)
   return state
+
+
+
+def reify(meaning, form):
+  if isinstance(form, basestring):
+    return walkstar(meaning, form)
+  if isinstance(form, tuple):
+    return tuple(reify(meaning, inner) for inner in form)
+  return form
+
+
+def walkstar(meaning, key):
+  key = walk(meaning, key)
+  if isinstance(key, tuple):
+    return tuple(walkstar(meaning, inner) for inner in key)
+  return key
+
+
+def walk(meaning, key):
+  while key in meaning:
+    means = meaning[key]
+    if means == key:
+      break
+    key = means
+  return key
+
+
+
+
+
+def FBA(a, b, Cin):
+  h = and_(a, b)
+  y = nor(h, nor(a, b))
+  j = and_(y, Cin)
+  return nor(j, nor(y, Cin)), or_(j, h)
+
+
+Sum, Cout = FBA('a', 'b', 'Cin')
+
+
+def make_adder(width):
+  sum_bits = []
+  Cin = 'Cin'
+  for i in range(width):
+    machine = {
+      'a': 'a' + str(i),
+      'b': 'b' + str(i),
+      'Cin': Cin,
+      }
+    sum_bits.append(reify(machine, Sum))
+    Cin = reify(machine, Cout)
+  return sum_bits, Cin
 
