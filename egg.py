674ef64749531dafc55657a23e6325c7bc524644
Simon Forman <forman.simon@gmail.com>
2013-04-17 11:39:25 -0700

    Minor changes.

--- egg.py
+++ egg.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from itertools import product, imap, izip
+from operator import eq
+from string import ascii_lowercase
 
 
 B = ((),), ()
@@ -18,19 +20,31 @@ nand = lambda *bits: nor(and_(bits))
 xor = lambda *bits: nor(and_(*bits), nor(*bits))
 
 
+# Forms with names.  (Tuples and strings.)
+
+
+Records = depths, counts, names = {}, {}, {}
+
+
 def unwrap(form):
   '''
   Remove all (()) and let ((*)) -> * in a form. Generator.
   '''
   for term in form:
+
     if term == ((),):
       continue
-    if isinstance(term, tuple) and len(term) == 1 and isinstance(term[0], tuple):
+
+    if (isinstance(term, tuple)
+        and len(term) == 1
+        and isinstance(term[0], tuple)
+        ):
       # Flatten out one "layer" of "wrapping".
       for item in term[0]:
         yield item
-    else:
-      yield term
+      continue
+
+    yield term
 
 
 _A = lambda form: (form
@@ -51,6 +65,30 @@ def Reduce(form):
       and len(form[0]) == 1):
     return Reduce(form[0][0])
   return tuple(_A(_B(tuple(map(Reduce, form)))))
+
+
+def depth(form):
+  if isinstance(form, basestring):
+    return 0
+  return 1 + (max(depth(inner) for inner in form) if form else 0)
+
+
+def count(form):
+  if isinstance(form, basestring):
+    return 0
+  return 1 + sum(count(inner) for inner in form)
+
+
+def sort_key(form):
+  if isinstance(form, basestring):
+    return 0, form
+  return 1, depth(form), count(form)
+
+
+def normalize(form):
+  if isinstance(form, basestring):
+    return form
+  return tuple(sorted((normalize(inner) for inner in form), key=sort_key))
 
 
 def walk(meaning, name):
@@ -108,7 +146,51 @@ def solve(form):
   for m, r in exhaust(form):
     no_yes[mark(r)].append((m, r))
   return no_yes
-    
+
+
+def unify(u, v, s, eq=eq):
+  """
+  Find substitution so that u == v while satisfying s
+
+  >>> unify((1, x), (1, 2), {})
+  {x: 2}
+
+  """
+  u = walk(s, u)
+  v = walk(s, v)
+  if eq(u, v):
+    return s
+  if isinstance(u, basestring):
+    return assoc(s, u, v)
+  if isinstance(v, basestring):
+    return assoc(s, v, u)
+  if isinstance(u, tuple) and isinstance(v, tuple):
+    if len(u) != len(v):
+      return False
+    for uu, vv in zip(u, v):  # avoiding recursion
+      s = unify(uu, vv, s, eq)
+      if s == False: # (instead of a Substitution object.)
+        break
+    return s
+  return False
+
+
+def name_normalize(form, symbols=ascii_lowercase):
+  names = collect_names(form)
+  meaning = dict(izip(sorted(names), symbols))
+  return reify(meaning, form)
+
+
+def record(form):
+  form = normalize(Reduce(form))
+#  form = name_normalize(form)
+  if form in depths:
+    return form, depths[form], counts[form], names[form]
+  d = depths[form] = depth(form)
+  c = counts[form] = count(form)
+  n = names[form] = collect_names(form)
+  return form, d, c, n
+
 
 s = lambda term: (str(term)
                   .replace(' ', '')
--- sety.py
+++ sety.py
@@ -5,25 +5,3 @@ def convert(form):
   if isinstance(form, basestring):
     return form
   return frozenset(convert(inner) for inner in form)
-
-
-def depth(form):
-  if isinstance(form, basestring):
-    return 0
-  return 1 + (max(depth(inner) for inner in form) if form else 0)
-
-
-def count(form):
-  if isinstance(form, basestring):
-    return 0
-  return 1 + sum(count(inner) for inner in form)
-
-
-def sort_key(form):
-  return depth(form), count(form)
-
-
-def normalize(form):
-  if isinstance(form, basestring):
-    return form
-  return tuple(sorted(form, key=sort_key))
