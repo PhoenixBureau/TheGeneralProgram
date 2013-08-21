4ae25c89a920a6622b9ed9c92a91848bdf71619f
Simon Forman <forman.simon@gmail.com>
2013-08-21 11:26:21 -0700

    Fun with stochas/.

--- gmmm.py
+++ gmmm.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint as p_
 from random import choice, random, shuffle, sample
+from heapq import heapify, heappush
 from egg import *
 import sys
 sys.setrecursionlimit(200)
@@ -19,11 +20,6 @@ class LilMachine(object):
     self.last = 0
     self.seen = set()
     self._note()
-
-  def reset(self):
-    self.R = dict.fromkeys(self.R.keys(), ((),))
-    self.score = 0
-    self.seen.clear()
 
   def val(self):
      return sum(
@@ -48,31 +44,11 @@ class LilMachine(object):
     self.seen.add(value)
     return value
 
+  def __cmp__(self, other):
+    return cmp(self.score, other.score)
+
   def __repr__(self):
     return '<LM at %i: %s>' % (self.score, view_register(self.R))
-
-
-class Repro(object):
-
-  def __init__(self):
-    self.prob = 0.5
-
-  def hmm(self):
-    self.prob = max((self.prob - 0.001, 0.01))
-    return self.prob < random()
-
-  def reproduce(self, one, mate):
-    kid_p = []
-    kid_p.extend(one.P.items())
-    kid_p.extend(mate.P.items())
-    shuffle(kid_p)
-    kid_p = dict(kid_p)
-    alphabet = one.R.keys()
-    # Mutation
-    if one.P == mate.P or random() < 0.01:
-      k, expression = next(generate_program(1, alphabet))
-      kid_p[k] = expression
-    return LilMachine(alphabet, **kid_p)
 
 
 def reproduce(one, mate):
@@ -96,32 +72,8 @@ def generate_program(N, alphabet):
     yield choice(alphabet), Reduce(op(*args))
 
 
-def cyc(pop):
-  for lm in pop[:]:
-    try:
-      lm.next()
-    except StopIteration:
-      pop.remove(lm)
-
-
-def cyc2(pop):
-  for lm in pop[:]:
-    lm.reset()
-    for _ in lm:
-      pass
-    if lm.score < 1:
-      pop.remove(lm)
-
-
-def repopulate(N, pop, selection):
-  if len(pop) < N:
-    p = sorted(pop, reverse=True, key=selection)
-    p = p[:N - len(pop)]
-    for parent in p:
-      pop.append(reproduce(parent, choice(pop)))
-
-
 if __name__ == '__main__':
+  _f = sys.stdout.flush
 
   N, M = 10, 200
   K = list(ascii_lowercase[:N])
@@ -135,28 +87,63 @@ if __name__ == '__main__':
       pass
     if lm.score:
       population.append(lm)
+      print '.',
+      _f()
   print 'done.'
+  k = [lm.score for lm in population]
+  m = max(k)
+  av = sum(k) / float(M)
+  print 'max: %i  min: %i  average: %4g' % (m, min(k), av)
 
-  Gens = 0
-  while max(lm.score for lm in population) < 1024:
+  population.sort()
 
-    population.sort(key=lambda lm: lm.score)
-    del population[:M / 4]
-#    winners = population[M / 4:]
+  def cyc():
+##    av = sum(lm.score for lm in population) / float(M)
+    m = max(lm.score for lm in population)
+    population.pop(0)
+    while True:
+      x = reproduce(
+        choice(population),
+        LilMachine(K, **dict(generate_program(5, K)))
+        )
+      for v in x:
+        pass
+      if x.score >= m:
+        print x, x.score
+        heappush(population, x)
+        break
 
-    while len(population) < M:
-      population.sort(key=lambda lm: lm.score)
-      winners = population[M / 4:]
+  av = sum(lm.score for lm in population) / float(M)
+  while av < 12:
+    cyc()
+    av = sum(lm.score for lm in population) / float(M)
+    print av, max(lm.score for lm in population)
+    _f()
 
-      # And a sport for luck.
-      winners.append(LilMachine(K, **dict(generate_program(5, K))))
+##  sort_key = lambda lm: lm.score
 
-      for p in winners:
-        k = reproduce(p, choice(population))
-        for _ in k: pass
-        if k.score > p.score:
-          population.append(k)
+#  population.sort()#key=sort_key)
+##  Gens = 0
 
-    print Gens, max(lm.score for lm in population)
-    Gens += 1
+#  while m < 100:
+##    del population[:M / 4]
+##  winners = population[M / 4:]
+##  for w in winners:
+##    print w
+##
+##    while len(population) < M:
+##      population.sort(key=lambda lm: lm.score)
+##      winners = population[M / 4:]
+##
+##      # And a sport for luck.
+##      winners.append(LilMachine(K, **dict(generate_program(5, K))))
+##
+##      for p in winners:
+##        k = reproduce(p, choice(population))
+##        for _ in k: pass
+##        if k.score > p.score:
+##          population.append(k)
+##
+##    print Gens, max(lm.score for lm in population)
+##    Gens += 1
 
