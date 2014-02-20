442d3b23996081521091be14c4dff9bce2562fb6
Simon Forman <sforman@hushmail.com>
2014-02-20 12:33:31 -0800

    Parameterize the program generation.

--- runtime.py
+++ runtime.py
@@ -1,24 +1,35 @@
 from random import choice
-from string import ascii_lowercase as alphabet
+from string import ascii_lowercase
 from egg import xor, and_, and_
 from machine import Machine, _, o, view_program
 
 
-program = {
-  letter: xor(
-    and_(
-      letter,
-      choice(alphabet),
-      ),
-    alphabet[(i + 1) % len(alphabet)],
-    )
-  for i, letter in enumerate(alphabet)
-  }
+def generate_program(alphabet, per_bit):
+  return {
+    letter: per_bit(i, letter, alphabet)
+    for i, letter in enumerate(alphabet)
+    }
+
+
+def generate_xor_ring_program(alphabet):
+  '''
+  A simple program that XORs a bit with its next neighbor.
+  
+  '''
+  def per_bit(i, letter, A):
+    return xor(letter, A[(i + 1) % len(A)])
+  return generate_program(alphabet, per_bit)
+
+  ##    and_(letter, choice(alphabet)),
+
+
+A = ascii_lowercase[:8]
+program = generate_xor_ring_program(A)
 print view_program(program)
 
 
-m = Machine(alphabet, program)
-m.R['z'] = o
+m = Machine(A, program)
+m.R[A[-1]] = o
 
 
 n, looped_to, seen = m.find_cycle(noisy=True)
