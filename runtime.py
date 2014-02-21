0a131aca5f04153838349fe503fe269283e036dd
Simon Forman <sforman@hushmail.com>
2014-02-20 17:34:00 -0800

    Minor cleanup.

--- runtime.py
+++ runtime.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from random import choice
 from string import ascii_lowercase
 from egg import xor, and_, and_
@@ -11,27 +12,33 @@ def generate_program(alphabet, per_bit):
     }
 
 
-def generate_xor_ring_program(alphabet):
-  '''
-  A simple program that XORs a bit with its next neighbor.
-  
-  '''
-  def per_bit(i, letter, A):
-    return xor(letter, A[(i + 1) % len(A)])
-  return generate_program(alphabet, per_bit)
-
   ##    and_(letter, choice(alphabet)),
 
 
-A = ascii_lowercase[:8]
-program = generate_xor_ring_program(A)
+def enprogramate(per_bit): # Brutal decorator abuse ensues.
+  @wraps(per_bit)
+  def gen_prog(alphabet):
+    return generate_program(alphabet, per_bit)
+  return gen_prog
+
+
+@enprogramate
+def xor_ring_program(i, letter, alphabet):
+  '''
+  A simple program that XORs a bit with its next neighbor.
+  Circular.  Makes Sierpinsk Gasket.  (Try with alphabets
+  with various lengths, i.e. not powers of two.)
+  '''
+  return xor(letter, alphabet[(i + 1) % len(alphabet)])
+
+
+A = ascii_lowercase[:10]
+
+
+program = xor_ring_program(A)
 print view_program(program)
-
-
 m = Machine(A, program)
 m.R[A[-1]] = o
-
-
 n, looped_to, seen = m.find_cycle(noisy=True)
 
 
