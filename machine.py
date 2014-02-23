a16c4d15ddd3cce108e7c932f4ec41f714e32621
Simon Forman <sforman@hushmail.com>
2014-02-23 14:45:18 -0800

    A very simple countinng "computer".

--- machine.py
+++ machine.py
@@ -1,3 +1,4 @@
+from egg import Reduce, normalize
 from void import void, reify, pretty
 
 
@@ -16,10 +17,10 @@ class Machine(object):
     self.P = program
 
   def cycle(self):
-    next_values = self.R.copy()
-    for bit, expression in self.P.iteritems():
-      next_values[bit] = reduce_(reify(expression, self.R))
-    self.R.update(next_values)
+    self.R.update({
+      bit: reduce_(reify(expression, self.R))
+      for bit, expression in self.P.iteritems()
+      })
 
   def __repr__(self):
     return '[%s]' % (view_register(self.R),)
@@ -27,18 +28,30 @@ class Machine(object):
   def find_cycle(self, noisy=False):
     seen, n = {}, 0
     while True:
-      s = str(self)
-      if s in seen:
-        return n, seen[s], sorted(seen, key=seen.get)
+      i = self.as_int()
+      if i in seen:
+        return n, seen[i], sorted(seen, key=seen.get)
       if noisy:
-        print ('%3i' % n), s
-      seen[s] = n
+        print ('%3i' % n), self
+      seen[i] = n
       n += 1
       self.cycle()
 
   def save(self, file_ish):
     import pickle
     pickle.dump(self, file_ish)
+
+  def reset(self):
+    for bit in self.R:
+      self.R[bit] = _
+
+  def as_int(self):
+    g = (
+      (n, self.R[bit])
+      for n, bit in
+      enumerate(sorted(self.R, reverse=True))
+      )
+    return sum(2**n for n, bit in g if not bit)
 
 
 def view_register(r):
@@ -52,3 +65,35 @@ def view_program(p):
      for bit in sorted(p)
     )
 
+
+nor = lambda *bits: bits
+or_ = lambda *bits: (bits,)
+and_ = lambda *bits: tuple((bit,) for bit in bits)
+nand = lambda *bits: (and_(*bits),)
+xor = lambda x, y: (((x, (y,)), ((x,), y)),)
+
+
+ifthen = lambda q, a, b: ((a, (q,)), (q, b))
+flipflop = lambda q, r, s: ((s, q), r)
+
+
+def FBA(a, b, c):
+  '''Full Bit Adder.'''
+  k = xor(a, b)
+  return xor(k, c), or_(and_(k, c), and_(a, b))
+
+
+def make_adder(reg0, reg1):
+  if len(reg0) != len(reg1):
+    raise ValueError
+  bits, carry = [], _
+  for r0, r1 in zip(reg0, reg1):
+    sum_bit, carry = FBA(r0, r1, carry)
+    bits.append(normalize(Reduce(sum_bit)))
+  return bits, normalize(Reduce(carry))
+
+
+def make_switch(q, reg0, reg1):
+  if len(reg0) != len(reg1):
+    raise ValueError
+  return tuple(ifthen(q, r0, r1) for r0, r1 in zip(reg0, reg1))
--- show_addition.py
+++ show_addition.py
@@ -1,36 +1,31 @@
 #!/usr/bin/env python
 from string import ascii_lowercase
 from itertools import product
-from egg import xor, and_, or_, Reduce, normalize
-from machine import Machine, _, o, view_program
-
-
-def FBA(a, b, c):
-  '''Full Bit Adder.'''
-  k = xor(a, b)
-  return xor(k, c), or_(and_(k, c), and_(a, b))
-
-
-def make_adder(reg0, reg1):
-  if len(reg0) != len(reg1): raise ValueError
-  bits, carry = [], _
-  for r0, r1 in zip(reg0, reg1):
-    sum_bit, carry = FBA(r0, r1, carry)
-    bits.append(normalize(Reduce(sum_bit)))
-  return bits, normalize(Reduce(carry))
+from machine import (
+  Machine, _, o, view_program,
+  flipflop, make_adder, make_switch,
+  )
 
 
 a, b, c, d, w, x, y, z = 'abcdwxyz'
+oh, p, q, r, s, t = 'opqrst'
 reg0, reg1 = (a, b, c, d), (z, y, x, w)
-sum_bits, carry = make_adder(reg0, reg1)
-program = dict(zip(reg0, sum_bits))
-program['o'] = carry
+R, X, A = (r, s, t), (x, y, z), (a, b, c)
+
+
+program = {}
+sum_bits, program[oh] = make_adder(reg0, reg1)
+program.update(zip(reg0, sum_bits))
+program.update(zip(R, make_switch(p, X, A)))
+program[p] = flipflop(p, c, oh)
+
+
 print view_program(program)
 m = Machine(ascii_lowercase, program)
 
 
 B = _, o
-for m.R[x], m.R[y], m.R[z] in product(B, B, B):
-  m.R[a] = m.R[b] = m.R[c] = m.R[d] = m.R['o'] = _
-  print '    ', ascii_lowercase
+for m.R[w], m.R[x], m.R[y], m.R[z] in product(B, B, B, B):
+  print '    ', ascii_lowercase, m.as_int()
   n, looped_to, seen = m.find_cycle(noisy=True)
+  m.reset()
